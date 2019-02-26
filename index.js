becc3a7225f21941cd1d0c7d71de37ad2824c387
ddemarkk <ddemarkk@gmail.com>
2019-02-26 22:36:06 +0200

    cart added

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const cors = require('cors');
 const app = express();
 const book = require('./routers/book.router');
 const user = require('./routers/user.router');
+const cart = require('./routers/cart.router');
 require('./db/mongoose');
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use('/user', user)
+app.use('/user', user);
 app.use('/book', book);
+app.use('/cart', cart);
 
 module.exports = app;
